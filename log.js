bc6726f9ca0b89ff4ecdb56ce964c66d101a1d44
ceomsf786-bit <ceomsf786@gmail.com>
2025-11-17 20:54:40 +0200

    Update log.js

--- log.js
+++ log.js
@@ -3,7 +3,7 @@
 // ---------- CONFIG ----------
 const HOURS = Array.from({length:24},(_,i)=>i);
 const STORAGE_KEY = 'studylog_v1';
-const GOOGLE_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbxxsiNnh0sDA1r6k6RXEGZRarM3ju8BA5PnPulc1EFwx0RBvcNmLcsOv35JTCMmDPNZuw/exec'; // <-- REPLACE THIS
+const GOOGLE_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbxxsiNnh0sDA1r6k6RXEGZRarM3ju8BA5PnPulc1EFwx0RBvcNmLcsOv35JTCMmDPNZuw/exec"; // <-- REPLACE THIS
 
 // ---------- Helpers ----------
 function fmtDate(d){ return d.toISOString().slice(0,10); }
